6c2a96705fe9fcd46cd8b1ff7dc3da8118f6ce99
Julian Jelfs <julian.jelfs@gmail.com>
2015-03-21 10:07:04 +0000

    got basic version of google globe working

---
+++ v2/public/javascripts/app.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+if(!Detector.webgl){
+    Detector.addGetWebGLMessage();
+} else {
+
+    var years = ['1990','1995','2000'];
+    var container = document.getElementById('container');
+    var globe = new DAT.Globe(container, {imgDir:'images/'});
+
+    console.log(globe);
+    var i, tweens = [];
+
+    var xhr;
+    TWEEN.start();
+
+
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', '/data/population909500.json', true);
+    xhr.onreadystatechange = function(e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                window.data = data;
+                for (i=0;i<data.length;i++) {
+                    globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
+                }
+                globe.createPoints();
+                globe.animate();
+                document.body.style.backgroundImage = 'none'; // remove loading
+            }
+        }
+    };
+    xhr.send(null);
+}
+}
